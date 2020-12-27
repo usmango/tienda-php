4d38a1d4221cba326c172016d6f3d960ef2c9432
usmangoSOBREMESA <usmangomezabeuov@gmail.com>
2020-12-27 13:10:56 +0100

    second commit

---
+++ layout-header.php
@@ -0,0 +1,21 @@
+<header>
+
+    <nav>
+        <section>
+            <section>
+                <ul>
+                    <li><a href="">Iniciar sesión</a></li>
+                    <li><a href="">Registrarse</a></li>
+                </ul>
+            </section>
+            <img src="" alt="">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Categorias</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </section>
+    </nav>
+
+</header>
